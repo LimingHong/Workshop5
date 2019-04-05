de5591de66c22f78dd556b9a2aa7e03121b776d7
Guido Amaya Robayo <812406@ACDM.DS.SAIT.CA>
2019-04-05 12:52:56 -0600

    added booking bookingDetails classes and customerhistoryDB

---
+++ TravelExpertsClassLib/BookingDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExpertsClassLib
+{
+    public class BookingDetails
+    {
+        public int BookingDetailId { get; set; }
+        public float ItineraryNo { get; set; }
+        public DateTime TripStart { get; set; }
+        public DateTime? TripEnd { get; set; }
+        public string Description { get; set; }
+        public string Destination { get; set; }
+        public int BasePrice { get; set; }
+
+    }
+}
